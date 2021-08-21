012438d4215435d926174d12b0ab0cdd87c28b40
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-08-22 09:09:50 +1000

    Test exceptions using fractions

---
+++ practicals/prac_02/exceptions_demo.py
@@ -0,0 +1,14 @@
+# ValueError if a character that is not a number is entered
+# A ZeroDivisionError if divided by 0
+try:
+
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Cannot divide by zero!")
+        denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+print("Finished.")
