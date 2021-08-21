38b488227ed66f751539039bd0447d61624102c8
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-08-22 09:23:04 +1000

    add password check with requirements

---
+++ practicals/prac_02/password_checker.py
@@ -0,0 +1,57 @@
+"""
+CP1404/CP5632 - Practical
+Password checker "skeleton" code to help you get started
+"""
+
+MIN_LENGTH = 2
+MAX_LENGTH = 6
+SPECIAL_CHARS_REQUIRED = False
+SPECIAL_CHARACTERS: str = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
+
+
+def main():
+    """Program to get and check a user's password."""
+    print("Please enter a valid password")
+    print("Your password must be between {} and {} characters, and contain:".format(MIN_LENGTH, MAX_LENGTH))
+    print("\t1 or more uppercase characters")
+    print("\t1 or more lowercase characters")
+    print("\t1 or more numbers")
+    if SPECIAL_CHARS_REQUIRED:
+        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+    password = input("> ")
+    while not is_valid_password(password):
+        print("Invalid password!")
+        password = input("> ")
+    print("Your {}-character password is valid: {}".format(len(password),
+                                                           password))
+
+
+def is_valid_password(password):
+    """Determine if the provided password is valid."""
+
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
+
+    count_lower = 0
+    count_upper = 0
+    count_digit = 0
+    count_special = 0
+    for char in password:
+        if char.isdigit():
+            count_digit += 1
+        if char.isupper():
+            count_upper += 1
+        if char.islower():
+            count_lower += 1
+        if SPECIAL_CHARS_REQUIRED:
+            if char in SPECIAL_CHARACTERS:
+                count_special += 1
+    if SPECIAL_CHARS_REQUIRED:
+        if count_digit != 0 and count_upper != 0 and count_lower != 0 and count_special != 0:
+            return True
+    else:
+        if count_digit != 0 and count_upper != 0 and count_lower != 0:
+            return True
+
+
+main()
