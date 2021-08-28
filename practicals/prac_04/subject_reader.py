ca42db7dc1566545b3cdcb84706d9e24a2a267c9
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-08-29 07:51:41 +1000

    Add number of quick picks to make

--- practicals/prac_04/subject_reader.py
+++ practicals/prac_04/subject_reader.py
@@ -9,11 +9,13 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    display_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,8 +25,15 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
-
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def display_data(data):
+    """print data formatted neatly"""
+    for subject_data in data:
+        print("{} is taught by {:<12} and has {:>3} students".format(*subject_data))
 
 
 main()
