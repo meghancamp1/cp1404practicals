be7434ab53dd115018c9d2350ec363f5b7373db8
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-09-07 11:41:39 +1000

    add ProgrammingLanguage class

---
+++ prac_06/languages.py
@@ -0,0 +1,6 @@
+from prac_06.programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
