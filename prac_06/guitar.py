f5938abb148ba6c603c6eb8c1e79d7f072566333
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-09-07 13:15:22 +1000

    add Guitar class

---
+++ prac_06/guitar.py
@@ -0,0 +1,27 @@
+CURRENT_YEAR = 2021
+
+
+class Guitar:
+    """Represent a Guitar object"""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar object"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Make string of Guitar data"""
+        return "{} ({}) : ${:2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        """Get age for Guitar object"""
+        age = CURRENT_YEAR - self.year
+        return age
+
+    def is_vintage(self):
+        """Determines if Guitar object is over 50 years old"""
+        age = self.get_age()
+        if age >= 50:
+            return True
+        return False
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,19 +2,19 @@ class ProgrammingLanguage:
     """Represent a Programming Language object"""
 
     def __init__(self, name, typing, reflection, year):
-        """initialise a Programming Language object"""
+        """Initialise a Programming Language object"""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
-        """check if Programming Language object is dynamic"""
+        """Check if Programming Language object is dynamic"""
         if self.typing != "Dynamic":
             return False
         return True
 
     def __str__(self):
-        """make string of Programming Language object data"""
+        """Make string of Programming Language object data"""
         return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection,
                                                                           self.year)
