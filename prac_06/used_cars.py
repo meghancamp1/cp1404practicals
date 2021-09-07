fef182eef5bde9ae23f98217254c07dd78971add
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-09-07 11:17:47 +1000

    add name to car class

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,7 +6,7 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("Car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -14,6 +14,18 @@ def main():
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+    # modifications:
+    # no. 1
+    limo = Car("Limo", 100)
+    # no. 2
+    limo.add_fuel(20)
+    # no. 3
+    print("fuel =", limo.fuel)
+    # no. 4
+    limo.drive(115)
+    # no. 5
+    print("odo =", limo.odometer)
+    print(limo)
 
 
 main()
