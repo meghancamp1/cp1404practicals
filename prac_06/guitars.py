dd9d9321c5fb4d9473ab2741337740b5a3d53b1d
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-09-12 18:31:39 +1000

    Fix print statements and sort guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,9 +4,14 @@ print("My guitars!")
 guitars = []
 name = input("Name: ")
 while not name == "":
-    year = input("Year: ")
-    cost = input("Cost: $")
-    guitar_to_add = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitar_to_add = Guitar(name, year, cost)
     guitars.append(guitar_to_add)
     print(guitar_to_add, "added.")
     name = input("Name: ")
+print("These are my guitars:")
+guitars.sort()
+for count, guitar in enumerate(guitars, 1):
+    response = "(vintage)" if guitar.is_vintage() else ""
+    print("{}. {:>20} ({}), worth $ {:10,.2f} {}".format(count, guitar.name, guitar.year, guitar.cost, response))
