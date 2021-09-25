28e1b32a832b55b177566d6adfefa48fa19dadea
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-09-26 09:21:15 +1000

    Refactor function and remove initial text

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -4,26 +4,27 @@ from kivy.lang import Builder
 MILES_TO_KM = 1.60934
 
 
-class ConvertMilesKmApp(App):
+class ConvertMilesApp(App):
+
     def build(self):
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
 
     def handle_convert(self):
-        result = self.get_valid_miles() * MILES_TO_KM
-        self.root.ids.output_label.text = str(result)
+        result = self.convert_to_number() * MILES_TO_KM
+        self.root.ids.output_km.text = str(result)
 
-    def get_valid_miles(self):
+    def convert_to_number(self):
         try:
             value = float(self.root.ids.input_number.text)
             return value
         except ValueError:
-            return 0
+            return 0.0
 
     def handle_increment(self, increment_size):
-        result = self.get_valid_miles() + increment_size
+        result = self.convert_to_number() + increment_size
         self.root.ids.input_number.text = str(result)
 
 
-ConvertMilesKmApp().run()
+ConvertMilesApp().run()
