6933bde31569117f5f244cf158258bc636c8ebcd
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-10-08 13:31:16 +1000

    Add expected output in comments

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -2,7 +2,12 @@ from prac_08.taxi import Taxi
 
 prius_taxi = Taxi("Prius 1", 100)
 print(prius_taxi.drive(40))
+# should print 40
 print(prius_taxi, ", current fare is ${:.2f}".format(prius_taxi.get_fare()))
+# fare should be $49.20
 prius_taxi.start_fare()
 prius_taxi.drive(100)
+# should print 60
 print(prius_taxi, ", current fare is ${:.2f}".format(prius_taxi.get_fare()))
+# fare should be $73.80
+
