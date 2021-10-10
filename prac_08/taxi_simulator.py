cca896a5bda23df8e717db1adf92748a8db64922
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-10-10 17:53:54 +1000

    Add choose taxi option

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -6,16 +6,32 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    total_bill = 0
+    current_taxi = None
     print("Let's drive!")
     print(MENU)
     choice = input(">>> ").lower()
     while choice != "q":
         if choice == "c":
-            pass
+            display_taxis(taxis)
+            try:
+                taxi_choice = int(input("Choose taxi: "))
+                current_taxi = taxis[taxi_choice]
+            except IndexError or ValueError:
+                print("Invalid taxi choice")
         elif choice == "d":
             pass
         else:
             print("Invalid option")
+        print("Bill to date: ${:.2f}".format(total_bill))
         print(MENU)
         choice = input(">>> ").lower()
     print("Total trip cost: {}")
+
+
+def display_taxis(taxis):
+    for count, taxi in enumerate(taxis):
+        print(f"{count} - {taxi}")
+
+
+main()
