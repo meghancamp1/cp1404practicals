31de9cd698c2e298f645c5ec3bf222a3e9784fec
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-10-10 18:29:06 +1000

    Add drive taxi option

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -20,13 +20,22 @@ def main():
             except IndexError or ValueError:
                 print("Invalid taxi choice")
         elif choice == "d":
-            pass
+            if current_taxi:
+                current_taxi.start_fare()
+                distance_to_drive = float(input("Drive how far? "))
+                current_taxi.drive(distance_to_drive)
+                total_bill += current_taxi.get_fare()
+                print("your {} trip cost you ${:.2f}".format(current_taxi.name, current_taxi.get_fare()))
+            else:
+                print("You need to choose a taxi before you can drive")
         else:
             print("Invalid option")
         print("Bill to date: ${:.2f}".format(total_bill))
         print(MENU)
         choice = input(">>> ").lower()
-    print("Total trip cost: {}")
+    print("Total trip cost: {}".format(total_bill))
+    print("taxis are now:")
+    display_taxis(taxis)
 
 
 def display_taxis(taxis):
