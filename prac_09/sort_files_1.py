6c8e5a69b75364dd397462c377ac9a618a48d94d
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-10-23 18:58:09 +1000

    Remove unnecessary print statement

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -5,7 +5,6 @@ os.chdir("FilesToSort")
 
 for filename in os.listdir("."):
     extension_name = filename.split(".")[-1]
-    print(extension_name)
     try:
         os.mkdir(extension_name)
     except FileExistsError:
